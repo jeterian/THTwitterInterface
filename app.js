153934b933ab2e9933733916d14f0928811a418e
jeterian <jeterian@gmail.com>
2017-04-11 17:01:13 -0400

    js done

--- app.js
+++ app.js
@@ -19,3 +19,61 @@ const T = new Twit({
 // View Engine, Static Directory
 app.set('views', path.join(__dirname, "..", "views"));
 app.set('view engine', 'pug');
+app.use(bodyPar.json());
+app.use(bodyPar.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Router
+app.use('/', routes);
+
+// Middleware to get information from Twitter
+app.use(twitter.getUser({url: 'account/settings'}));
+app.use(twitter.getCredentials({url: 'account/verify_credentials'}));
+app.use(twitter.getRecentTweets({url: 'statuses/home_timeline', count: 5}));
+app.use(twitter.getFriends({url: 'friends/list', count: 5}));
+app.use(twitter.getDirectMessages({url: 'direct?messages', count: 5}));
+app.use(twitter.postTweet({url: 'statuses/update'}));
+
+// ERRORS
+	// 404
+
+	app.use((req, res, next) => {
+		const err = new Error('Page not found');
+		err.status = 404;
+		next(err);
+	});
+
+	// 500 
+	if (app.get('env') === 'development') {
+		app.use((err, req, res, next) => {
+			res.status(err.status || 500);
+			res.render('error', {
+				message: err.message,
+				error: err
+			});
+		});
+	}
+
+	app.use((err, req, res, next) => {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			status: err.status
+		});
+	});
+
+// Server
+app.listen(3000, function() {
+	console.log('Server is running on port 3000')
+});
+
+
+module.exports = app;
+
+
+
+
+
+
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,71 +1,79 @@
-(function() {
+'use strict';
 
-	'use strict';
+// General Setup: Plugins, Variables, Etc.
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyPar = require('body-parser');
+const twitter = require('./twitter.js');
+const routes = require('../routes/index.js');
+const config = require('./config.js');
+const Twit = require('twit');
+const T = new Twit({
+	consumer_key: config.consumer_key,
+	consumer_secret: config.consumer_secret,
+	access_token: config.access_token,
+	access_token_secret: config.access_token_secret
+});
 
-	// Set Up (Packages, variables, etc.)
-	const express = require('express');
-	const Twit = require('twit');
-	const pug = require('pug');
-	const config = require('./config.js');
+// View Engine, Static Directory
+app.set('views', path.join(__dirname, "..", "views"));
+app.set('view engine', 'pug');
+app.use(bodyPar.json());
+app.use(bodyPar.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-	var app = express();
-	var friends;
-	var timeLine;
-	var messages;
+// Router
+app.use('/', routes);
 
-	// View Engine, Directories for Static Resources and Templates
-	app.use(express.static(__dirname + '/public'));
-	app.set('view engine', 'pug');
-	app.set('views', __dirname + '/templates');
+// Middleware to get information from Twitter
+app.use(twitter.getUser({url: 'account/settings'}));
+app.use(twitter.getCredentials({url: 'account/verify_credentials'}));
+app.use(twitter.getRecentTweets({url: 'statuses/home_timeline', count: 5}));
+app.use(twitter.getFriends({url: 'friends/list', count: 5}));
+app.use(twitter.getDirectMessages({url: 'direct?messages', count: 5}));
+app.use(twitter.postTweet({url: 'statuses/update'}));
 
-	// Server
-	app.listen(3000, function() {
-		console.log("Server is running on port 3000.");
-	});
+// ERRORS
+	// 404
 
-	// Main Page Handler
-	app.get('/', function(req, res) {
-		req.timelineTweets.then(function(info) {
-			timeLine = info.data;
-		
-			req.directMessages.then(function(info) {
-				message = info.data;
-				
-				req.followingUsers.then(function(info) {
-					friends = info.data.users;
-
-					res.render(__dirname + '/templates/index.pug',)
-				})
-			})
-		})
-	});
-
-	// Receiving Data
 	app.use((req, res, next) => {
-		const login = twit.getUser(config);
-
-		req.directMessages = twit.getDirectMessages(login);
-		req.timelineTweets = twit.getTimeline(login);
-		req.followingUsers = twit.getFollowing(login);
-		next();
+		const err = new Error('Page not found');
+		err.status = 404;
+		next(err);
 	});
 
-	// Twit Plugin
-	function getLogin(config) {
-		return Twit(config);
+	// 500 
+	if (app.get('env') === 'development') {
+		app.use((err, req, res, next) => {
+			res.status(err.status || 500);
+			res.render('error', {
+				message: err.message,
+				error: err
+			});
+		});
 	}
 
-	function getTL(user) {
-		return user.get('statuses/user_timeline', { screen_name: user.screen_name, count: 5});
-	}
+	app.use((err, req, res, next) => {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			status: err.status
+		});
+	});
 
-	function getMessages(user) {
-		return user.get('direct_messages/sent', {screen_name: user.screen_name, count: 5});
-	}
+// Server
+app.listen(3000, function() {
+	console.log('Server is running on port 3000')
+});
 
-	function getFriends(user) {
-		return user.get('friends/list', { screen_name: user.screen_name, count: 5 });
-	}
 
-	
-})
+module.exports = app;
+
+
+
+
+
+
+
+
