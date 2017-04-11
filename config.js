e32892ed4bdff2fd1af6119632c52eb684612817
jeterian <jeterian@gmail.com>
2017-04-11 17:17:42 -0400

    revised partials, views

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    // https://apps.twitter.com/
-    consumer_key:         'DFc1Ahz3BlhfyksGMGWvQNrm2',
-    consumer_secret:      '7AnTJVM0UD7T4fyUtvarcgLd2egYZIfpJChCMvCO0eDuz9e7l2',
-    access_token:         '21505905-QHTyhq8iGBdUef3BIIg3GnPgYrEWHzg4Zm3Yx3AVO',
-    access_token_secret:  'y3oiGKWwAL6DlheOEMPQ8tMnsm1OUcLugczdZdaHe6Utv',
-    timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
-};
--- src/js/configtemplate.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    // https://apps.twitter.com/
-    consumerKey:         '...',
-    consumerSecret:      '...',
-    accessToken:         '...',
-    accessTokenSecret:  '...',
-    timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
-};
